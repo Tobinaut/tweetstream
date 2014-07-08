a1e7ae10c8466e7bf5a4b1d6b04eb81bb8ee6c21
VictorKostyukov <xferzx@gmail.com>
2014-07-08 12:02:06 +0400

    Добавлено время

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+Handlebars.registerHelper('time', function(time) {
+  return moment(time).fromNow();
+});
 $(document).ready(function() {
 	var compiledTemplate = Handlebars.compile($('#template').html());
 	for(var i in tweetData) {
 		$('.test').append(compiledTemplate(tweetData[i]));	
 	}
 });
+
