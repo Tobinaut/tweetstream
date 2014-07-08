f573e63df7fefdd7894f21448ccbeef453fb0dbf
VictorKostyukov <xferzx@gmail.com>
2014-07-08 13:16:50 +0400

    Добавлена статистика

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 Handlebars.registerHelper('time', function(time) {
   return moment(time).fromNow();
 });
+
+function find(search_str, obj) {
+	return obj.text.indexOf(search_str) != -1;
+};
 $(document).ready(function() {
-	var compiledTemplate = Handlebars.compile($('#template').html());
+	var compiledTemplateStatistic = Handlebars.compile($('#statistic-template').html());
+	var search_str = 'кошка';
+	var statsObj = {
+		count:_.filter(tweetData, find.bind(null, search_str)).length, 
+		search: search_str
+	};
+	$('.statistic').append(compiledTemplateStatistic(statsObj));
+	var compiledTemplateTweets = Handlebars.compile($('#tweet-template').html());
 	for(var i in tweetData) {
-		$('.test').append(compiledTemplate(tweetData[i]));	
+		$('.tweet').append(compiledTemplateTweets(tweetData[i]));	
 	}
 });
 
