97f2a4d119a5ccc8af5cf6a4cb3731d305d9a99e
VictorKostyukov <xferzx@gmail.com>
2014-07-08 11:32:13 +0400

    Шаблоны в документе

--- script.js
+++ script.js
@@ -5,14 +5,9 @@ var tweetArray = [
 	{text: "bla-bla", author: "me"},
 ];
 
-var template = '<div><h2>{{author}}: </h2><p>"{{text}}"<p></div>';
-var compiledTemplate = Handlebars.compile(template); 
-
 $(document).ready(function() {
+	var compiledTemplate = Handlebars.compile($('#template').html());
 	for(var i in tweetArray) {
-		//var el = $('<div>');
-		var el = $('<div>');
-		//el.text(tweetArray[i].text);
 		$('.test').append(compiledTemplate(tweetArray[i]));	
 	}
 });
