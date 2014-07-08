f29f0e002715a7422e244b6f0ba4328f52ced19c
VictorKostyukov <xferzx@gmail.com>
2014-07-08 17:14:15 +0400

    карта и красивости

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 Handlebars.registerHelper('time', function(time) {
   return moment(time).fromNow();
 });
-
+function init(){     
+	var myMap = new ymaps.Map("map", {
+		center: [55.76, 37.64],
+		zoom: 7
+	});
+	var compactArr = _.compact(_.pluck(tweetData, 'geo'));
+	for(var i in compactArr) {
+		var myPlacemark = new ymaps.Placemark([compactArr[i].coordinates[0], compactArr[i].coordinates[1]], {});
+		myMap.geoObjects.add(myPlacemark);
+	}
+	
+	var myPlacemark = new ymaps.Placemark([55.76, 37.64], { 
+            hintContent: 'Москва!', 
+            balloonContent: 'Столица России' 
+        });
+};
 $(document).ready(function() {
 	var compiledTemplateStatistic = Handlebars.compile($('#statistic-template').html());
 	var search_str = 'кошка';
@@ -22,5 +37,9 @@ $(document).ready(function() {
 	$('.del-button').on('click', function () {
 		$(this).closest('.tweet-css').remove();
 	});
+	ymaps.ready(init);
+	
+		
+    
 });
 
